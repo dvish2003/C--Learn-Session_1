38e99addda3a08db5a89b73d83a98bd7645b02d3
dvish2003 <vishanchathuranga63@gmail.com>
2026-02-26 20:33:59 +0530

    c

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using FirstApp;
 
 class Program
@@ -76,14 +77,87 @@ class Program
         List<string> names = new List<string>();
         names.Add("Alice");
         names.Add("Bob");
-        names.Add("Charlie");   
+        names.Add("Charlie");
         Console.WriteLine("Names in the list:");
         foreach (string name in names)
         {
             Console.WriteLine(name);
         }
 
+        //LINQ query ====================================================================
+        //filters number
+
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 343, 23, 12, 3, 78, 98, 65, 22, 4, 55, 88 };
+        //where
+        var evenNumbers = numbers.Where(n => n % 2 == 0);
+        Console.WriteLine("Even numbers:");
+        foreach (int num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+
+        //select
+        var doubledNumbers = numbers.Select(n => n * 2);
+        Console.WriteLine("Doubled numbers:");
+        foreach (int num in doubledNumbers)
+        {
+            Console.WriteLine(num);
+        }
         
 
-    }
+
+        //First
+        var firstEven = numbers.First(n => n % 2 == 0);
+        Console.WriteLine("First even number: " + firstEven);
+
+        //order by
+        var orderedNumbers = numbers.OrderBy(n => n);
+        Console.WriteLine("Ordered numbers:");
+        foreach (int num in orderedNumbers)        {
+            Console.WriteLine(num);
+        }
+
+        //Any by
+        bool hasEven = numbers.Any(n => n % 2 == 0);
+        Console.WriteLine("Contains even numbers: " + hasEven);
+
+
+        //real worl example LINQ
+        List<Student> students = new List<Student>
+        {
+            new Student("Alice", 12),
+            new Student("Bob", 14),
+            new Student("Charlie", 19),
+            new Student("David", 21),
+            new Student("Eve", 20),
+            new Student("Frank", 18),
+            new Student("Grace", 11),
+            new Student("Heidi", 22),
+            new Student("Ivan", 2),
+            new Student("Judy", 21)
+        };
+
+
+        var eligibleListStudent = students.Where(s => s.age >= 18).Select(s => s.name).ToList();
+        Console.WriteLine("Eligible students for the program:");
+        foreach (string studentName in eligibleListStudent)        {
+            Console.WriteLine(studentName);
+        }
+
+
+
+        //orderby
+        List<int> numbers_1 = new List<int> { 5, 2, 9, 1, 5, 6 };
+        var sortedNumbers = numbers_1.OrderBy(n => n);
+        Console.WriteLine("Sorted numbers:");
+        foreach (int num in sortedNumbers)        {
+            Console.WriteLine(num);
+        }
+
+
+
+            //====================================================================================================
+       
+}
 }
